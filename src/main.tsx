c0d19103f7fad9a01aeadfc4374c1774f5bd94f2
ohdal <yoonhj97@naver.com>
2024-03-12 21:22:38 +0900

    feat: Github Pages 호스팅 관련 build 스크립트 분리 및 mode에 따른 router 분리

--- src/main.tsx
+++ src/main.tsx
@@ -7,18 +7,20 @@ import QuizPage from "./pages/QuizPage";
 import NotePage from "./pages/NotePage";
 import ResultPage from "./pages/ResultPage";
 
-const routes = createBrowserRouter(
-  [
-    { path: "/", element: <HomePage /> },
-    { path: "/quiz", element: <QuizPage /> },
-    { path: "/result", element: <ResultPage /> },
-    { path: "/note", element: <NotePage /> },
-  ],
-  { basename: "/QuizApp" }
-);
+const mode = import.meta.env.MODE;
+
+const router_base = [
+  { path: "/", element: <HomePage /> },
+  { path: "/quiz", element: <QuizPage /> },
+  { path: "/result", element: <ResultPage /> },
+  { path: "/note", element: <NotePage /> },
+];
+
+const router = createBrowserRouter(router_base);
+const routerGit = createBrowserRouter(router_base, { basename: "/QuizApp" });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <RouterProvider router={mode === "github" ? routerGit : router} />
   </React.StrictMode>
 );
